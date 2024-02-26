0693a2d686872fb5318c5b04f039165a31bf2863
r1segod <weezluver@gmail.com>
2024-02-26 10:52:54 +0300

    Выполнил практическое задание

--- task_3/main.js
+++ task_3/main.js
@@ -1,38 +1,17 @@
-
-let product = {
-  name: "Smartphone",
-  price: 500,
-  inStock: true,
-  brand: "Samsung",
-  specifications: {
-    screenSize: "6 inches",
-    camera: "12 MP",
-    memory: "128 GB"
-  },
-  discount: {
-    percentage: 10,
-    expirationDate: "2023-12-31"
-  }
+const pet = {
+  species: 'Cat',
+  name: 'Anfisa',
+  age: 5,
+  characteristics: ['playful', 'affectionate', 'curious'],
+  health: 'good'
 };
 
-let customer = {
-  name: "John",
-  budget: 600
-};
+for (let key in pet) {
+  console.log(`${key}: ${pet[key]}`);
+}
 
-if (customer.budget < product.price) {
-  console.log("У вас недостаточно средств для покупки этого товара.");
+if (pet.age < 5) {
+  console.log('Anfisa is still young!');
 } else {
-  console.log(`Вы можете приобрести ${product.name} по цене ${product.price} долларов.`);
-
-  console.log("Технические характеристики:");
-  for (let key in product.specifications) {
-    console.log(`${key}: ${product.specifications[key]}`);
-  }
-
-  if (product.discount) {
-    console.log(`Скидка ${product.discount.percentage}% до ${product.discount.expirationDate}`);
-    let discountedPrice = product.price - (product.price * product.discount.percentage / 100);
-    console.log(`Цена со скидкой: ${discountedPrice} долларов.`);
-  }
+  console.log('Anfisa is a mature cat.');
 }
